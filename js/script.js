7d831be84beed858271ab7c200a4190ffe25e7f4
lounikiforovski <lou@nikiforovski.com>
2015-02-13 21:46:35 -0500

    Updates to home

--- js/script.js
+++ js/script.js
@@ -7,6 +7,9 @@ $(function() {
 	$(".signup-btn").click(function(e) {
 		window.location.href = "https://drones.dronopy.com/en/signup";
 	});
+	$(".find-btn").click(function(e) {
+		window.location.href = "https://drones.dronopy.com/";
+	});
 	$("#alert-close").click(function(e) {
 		hideAlert();
 	});
